5d768d3ba5437a06176b9a103d40299c92e2ca1d
je-swy <kateryna.shemonaieva@gmail.com>
2026-02-23 19:59:04 +0200

    add: section 8 & modal feat

--- src/script/app.js
+++ src/script/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const burgerBtn = document.querySelector('header button.burger');
   const burgerBtnIcons = document.querySelectorAll('header button.burger img');
 
+  // Toggle mobile menu visibility and burger button icons
   burgerBtn.addEventListener('click', function () {
     burgerBtnIcons.forEach((icon) => {
       icon.classList.toggle('hidden');
@@ -16,12 +17,14 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('header .mobile-menu .features .search').classList.toggle('expanded');
   })
 
+  // Initialize Glide.js carousel
   new Glide('.sec5-cards', {
     type: 'carousel',
     startAt: 0,
     perView: 1
   }).mount();
 
+  // Load more cards logic
   const loadMoreProductCards = document.getElementById('load-more-cards');
   const productCards = document.querySelectorAll('.sec-4-card');
   let visibleCards = getVisibleCardValue();
@@ -57,6 +60,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  // Function to hide cards and scroll back to the section
   function hideCards() {
     activeVisibleCards = visibleCards;
 
@@ -75,17 +79,47 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  // Add resize event listener with debounce to recalculate visible cards
   window.addEventListener('resize', debounce(() => {
     visibleCards = getVisibleCardValue();
     activeVisibleCards = visibleCards;
     cardToOpen = visibleCards / 2;
   }, 200));
 
+  // Add click event listener to the load more button
   loadMoreProductCards.addEventListener('click', function () {
     if (activeVisibleCards < productCards.length) {
       showCards();
     } else {
       hideCards();
     }
+  });
+
+  // Modal logic
+  const btnIdsToOpenModal = ['sec-8-card-1-btn', 'sec-8-card-2-btn']; // Add IDs of buttons that should open modals
+  const generalModalWrapper = document.querySelector('.modals-wrapper');
+  const closeModalBtns = document.querySelectorAll('.modal-close-btn');
+
+  btnIdsToOpenModal.forEach((btnId) => {
+    // Get the button element and corresponding modal element
+    const btnEl = document.getElementById(btnId);
+    const modalId = btnEl.getAttribute('data-modal-id');
+    const modalEl = document.getElementById(modalId);
+
+    // Add click event listener to the button to open the modal
+    btnEl.addEventListener('click', function () {
+      generalModalWrapper.classList.add('visible');
+      modalEl.classList.add('visible');
+      document.querySelector('body').classList.add('overflow');
+    });
+  })
+
+  // Add click event listeners to all close buttons to close the modal
+  closeModalBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener('click', function () {
+      generalModalWrapper.classList.remove('visible');
+      this.parentElement.classList.remove('visible');
+      document.querySelector('body').classList.remove('overflow');
+    })
   });
 });
