8005c9147dbd9eb080336fb5db6386f955c368fe
je-swy <kateryna.shemonaieva@gmail.com>
2026-02-24 14:03:47 +0200

    add: functionality

--- src/script/app.js
+++ src/script/app.js
@@ -1,4 +1,190 @@
+const productInfo = [
+  {
+    id: 1,
+    category: 'Vegetable',
+    imgSrc: './assets/images/product-photo-1.png',
+    title: 'Calabrese Broccoli',
+    price: '$20.00',
+    discountPrice: '$13.00',
+    rating: 4.7
+  },
+  {
+    id: 2,
+    category: 'Fresh',
+    imgSrc: './assets/images/product-photo-2.png',
+    title: 'Fresh Banana Fruits',
+    price: '$20.00',
+    discountPrice: '$14.00',
+    rating: 5,
+  },
+  {
+    id: 3,
+    category: 'Millets',
+    imgSrc: './assets/images/product-photo-3.png',
+    title: 'White Nuts',
+    price: '$20.00',
+    discountPrice: '$15.00',
+    rating: 4.5,
+  },
+  {
+    id: 4,
+    category: 'Vegetable',
+    imgSrc: './assets/images/product-photo-4.png',
+    title: 'Vegan Red Tomato',
+    price: '$20.00',
+    discountPrice: '$17.00',
+    rating: 4,
+  },
+  {
+    id: 5,
+    category: 'Health',
+    imgSrc: './assets/images/product-photo-5.png',
+    title: 'Mung Bean',
+    price: '$20.00',
+    discountPrice: '$11.00',
+    rating: 3.3,
+  },
+  {
+    id: 6,
+    category: 'Nuts',
+    imgSrc: './assets/images/product-photo-6.png',
+    title: 'Brown Hazelnut',
+    price: '$20.00',
+    discountPrice: '$12.00',
+    rating: 4.6,
+  },
+  {
+    id: 7,
+    category: 'Fresh',
+    imgSrc: './assets/images/product-photo-7.png',
+    title: 'Eggs',
+    price: '$20.00',
+    discountPrice: '$17.00',
+    rating: 4.3,
+  },
+  {
+    id: 8,
+    category: 'Fresh',
+    imgSrc: './assets/images/product-photo-8.png',
+    title: 'Zelco Suji Elaichi Rusk',
+    price: '$20.00',
+    discountPrice: '$15.00',
+    rating: 4.7,
+  },
+  {
+    id: 9,
+    category: 'Fruit',
+    imgSrc: './assets/images/product-photo-9.png',
+    title: 'Organic Apples',
+    price: '$25.00',
+    discountPrice: '$20.00',
+    rating: 4.9,
+  },
+  {
+    id: 10,
+    category: 'Dairy',
+    imgSrc: './assets/images/product-photo-10.png',
+    title: 'Greek Yogurt',
+    price: '$15.00',
+    discountPrice: '$10.00',
+    rating: 4.8,
+  },
+  {
+    id: 11,
+    category: 'Vegetable',
+    imgSrc: './assets/images/product-photo-11.png',
+    title: 'Fresh Spinach',
+    price: '$18.00',
+    discountPrice: '$12.00',
+    rating: 4.6,
+  },
+  {
+    id: 12,
+    category: 'Grains',
+    imgSrc: './assets/images/product-photo-12.png',
+    title: 'Brown Rice',
+    price: '$22.00',
+    discountPrice: '$16.00',
+    rating: 4.4,
+  },
+  {
+    id: 13,
+    category: 'Herbs',
+    imgSrc: './assets/images/product-photo-13.png',
+    title: 'Fresh Basil',
+    price: '$10.00',
+    discountPrice: '$8.00',
+    rating: 4.7,
+  },
+  {
+    id: 14,
+    category: 'Snack',
+    imgSrc: './assets/images/product-photo-14.png',
+    title: 'Trail Mix',
+    price: '$18.00',
+    discountPrice: '$14.00',
+    rating: 4.5,
+  },
+  {
+    id: 15,
+    category: 'Beverage',
+    imgSrc: './assets/images/product-photo-15.png',
+    title: 'Green Tea',
+    price: '$12.00',
+    discountPrice: '$9.00',
+    rating: 4.8,
+  },
+  {
+    id: 16,
+    category: 'Frozen',
+    imgSrc: './assets/images/product-photo-16.png',
+    title: 'Frozen Berries',
+    price: '$15.00',
+    discountPrice: '$11.00',
+    rating: 4.6,
+  }
+];
+
 document.addEventListener('DOMContentLoaded', function () {
+  const mainProductContainer = document.querySelector('.sec-4-cards');
+  const anotherProductContainer = document.querySelector('.sec-6-cards');
+
+  function createProductCard(product, classOfSection) {
+    const activeRatingWidth = (product.rating * 100) / 5;
+    return `
+      <div class="sec-${classOfSection}-card product-card" data-product-id="${product.id}">
+        <p class="badge">${product.category}</p>
+        <div class="photo">
+          <img src="${product.imgSrc}">
+        </div>
+        <p class="title">${product.title}</p>
+        <div class="info df align-e justify-b">
+          <p class="price">
+            <span class="full">${product.price}</span>
+            <span class="discount">${product.discountPrice}</span>
+          </p>
+          <div class="rating">
+            <span class="default">★★★★★</span>
+            <span class="active" style="width: ${activeRatingWidth}%">★★★★★</span>
+          </div>
+        </div>
+        <a href="#" class="buy">Add to cart</a>
+      </div>
+    `;
+  }
+
+  function renderProducts(productContainer, products, classOfSection) {
+    products.forEach(product => {
+      const productCard = createProductCard(product, classOfSection);
+
+      productContainer.innerHTML += productCard;
+    });
+  }
+
+  renderProducts(mainProductContainer, productInfo, 4); // Render all products in the main section with class sec-4
+  renderProducts(anotherProductContainer, productInfo.slice(5, 9), 6); // Render a subset of products in another section with class sec-6 with id 6-9
+
+  // Mobile menu logic
   const burgerBtn = document.querySelector('header button.burger');
   const burgerBtnIcons = document.querySelectorAll('header button.burger img');
 
@@ -96,7 +282,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // Modal logic
-  const btnIdsToOpenModal = ['sec-8-card-1-btn', 'sec-8-card-2-btn']; // Add IDs of buttons that should open modals
+  const btnIdsToOpenModal = ['sec-8-card-1-btn', 'sec-8-card-2-btn', 'cart-modal-btn']; // Add IDs of buttons that should open modals
   const generalModalWrapper = document.querySelector('.modals-wrapper');
   const closeModalBtns = document.querySelectorAll('.modal-close-btn');
 
@@ -130,12 +316,20 @@ document.addEventListener('DOMContentLoaded', function () {
     return emailValidateRegExp.test(value);
   }
 
-  emailjs.init({
-    publicKey: "g3a8K7C6xKrDLKvqj",
-  });
+
+  (function () {
+    emailjs.init({
+      publicKey: "g3a8K7C6xKrDLKvqj",
+    });
+  })();
 
   const formBtn = document.getElementById('form-submit');
   const emailField = document.getElementById('email-input');
+
+  const notifier = new AWN({
+    position: 'bottom-right',
+    maxNotifications: 3
+  });
 
   formBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -144,20 +338,89 @@ document.addEventListener('DOMContentLoaded', function () {
     const isCurrentEmailValid = isEmailValid(currentEmail);
 
     if (isCurrentEmailValid) {
-      emailjs
-        .send("service_473vmpj", "template_1wsqmkp", {
-          user_email: currentEmail,
-        })
-        .then(function (response) {
-          console.log("SUCCESS!", response.status, response.text);
-          emailField.value = '';
-        })
-        .catch(function (error) {
-          console.log("FAILED...", error);
-        });
-      emailField.value = ''; //clear form after submiting
+      emailjs.send("service_473vmpj", "template_1wsqmkp", {
+        user_email: currentEmail,
+      });
+      emailField.value = '';
     } else {
-      console.log('Please type correct email adress!')
+      notifier.alert('Please type correct email address!');
     }
-  })
+  });
+
+  // Cart logic
+  let selectedProducts = JSON.parse(localStorage.getItem('selectedProducts')) || [];
+  const allBuyButtons = document.querySelectorAll('.product-card .buy');
+
+  // Function to update cart value indicators and button states
+  function updateCartValueIndicators() {
+    const allSelectedProducts = JSON.parse(localStorage.getItem('selectedProducts')) || [];
+    const cartCountElement = document.querySelector('header .features .cart p span');
+    cartCountElement.textContent = allSelectedProducts.length;
+
+    allBuyButtons.forEach(buyBtn => {
+      const productId = buyBtn.parentElement.getAttribute('data-product-id');
+      buyBtn.classList.toggle('active', allSelectedProducts.includes(productId));
+    });
+  }
+
+  // Function to handle buy button clicks, toggle product selection, and update localStorage
+  function handleBuyButtonClick(event) {
+    event.preventDefault();
+
+    const productId = this.parentElement.getAttribute('data-product-id');
+    this.classList.toggle('active');
+
+    if (this.classList.contains('active')) {
+      selectedProducts.push(productId);
+    } else {
+      selectedProducts = selectedProducts.filter(id => id !== productId);
+    }
+
+    localStorage.setItem('selectedProducts', JSON.stringify(selectedProducts));
+    updateCartValueIndicators();
+  }
+
+  // Function to display selected products in the cart modal and add event listeners for removing products from the list
+  function displaySelectedProducts() {
+    const productList = document.querySelector('#cart-modal ul');
+    productList.innerHTML = selectedProducts.map(productId => {
+      const productName = getProductNameById(productId);
+      // Return the list item HTML with a data attribute for the product ID and a remove button
+      return `
+        <li data-product-id="${productId}">
+          <span>${productName}</span>
+          <i class="fa-regular fa-circle-xmark fa-lg" style="color: #274c5b;"></i>
+        </li>
+      `;
+    }).join('');
+
+    // Add click event listeners to the remove buttons to allow removing products from the list
+    selectedProducts.forEach(productId => {
+      const listItem = productList.querySelector(`li[data-product-id="${productId}"]`);
+      listItem.addEventListener('click', () => removeProductFromList(productId));
+    });
+  }
+
+  // Helper function to get product name by ID from the productInfo array
+  function getProductNameById(productId) {
+    const product = productInfo.find(p => p.id == productId);
+    return product ? product.title : 'Unknown Product';
+  }
+
+  // Function to remove a product from the selected products list, update localStorage, and refresh the cart display
+  function removeProductFromList(productId) {
+    // Remove the product ID from the selectedProducts array
+    selectedProducts = selectedProducts.filter(id => id !== productId);
+    localStorage.setItem('selectedProducts', JSON.stringify(selectedProducts));
+    updateCartValueIndicators();
+    displaySelectedProducts();
+  }
+
+  // Initial display of selected products in the cart modal and update cart indicators on page load
+  updateCartValueIndicators();
+
+  // Add click event listener to the cart modal button to display selected products when the modal is opened
+  allBuyButtons.forEach(buyBtn => {
+    buyBtn.addEventListener('click', handleBuyButtonClick);
+  });
 });
